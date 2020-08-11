2c2a7f4bd9a282e2f1400729dc33856d08494e81
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2020-08-11 18:57:09 -0500

    Extracted the merge subroutine from within the mergeSort() function and created a merge() function

--- I. Introduction/MergeSort.cpp
+++ I. Introduction/MergeSort.cpp
@@ -8,42 +8,48 @@ int main(){
     int array[] = {5,4,1,8,7,2,6,3};
 }
 
-int * mergeSort(int array[]) {
+void mergeSort(int * array, int n) {
 
-    int n = sizeof(array);
+    //'n' is the size of the array.
     int m = n/2;
 
     //Declarando arrays: 
     //  * Dos de length: m = n/2; A y B.
-    int A[m];
-    int B[m];
+    int * A = new int[m];
+    int * B = new int [m];
 
     //Poblando arrays...
     //x es el indice para poblar A; y el indice para poblar B.
     int x = 0, y = m;
     for(int i = 0; i < m; i++){
-        A[i] = array[x];
-        B[i] = array[y];
+        *(A+i) = *(array+x);
+        *(B+i) = *(array+y);
         x++;
         y++;
     }
 
     if(m == 1) {
-        int i = 0, j = 0;
+        merge(A, B, n);
+    }
 
-        //  * Uno de length: n = 2 * m; C.
-        int * C = new int[n];
+    mergeSort(A);
+    mergeSort(B);
+}
 
-        for(int k = 0; k < n; k++){
-            if(A[i] < B[j]){
-                C[k] = A[i];
-                i++;
-            }
-            else{
-                C[k] = B[j];
-                j++;
-            }
+void merge(int * A, int * B, int n) {
+    int i = 0, j = 0;
+
+    //  * Uno de length: n = 2 * m; C.
+    int * C = new int[n];
+
+    for(int k = 0; k < n; k++){
+        if(*(A+i) < *(B+j)){
+            *(C+k) = *(A+i);
+            i++;
         }
-        return C;
+        else{
+            *(C+k) = *(B+j);
+            j++;
+        }
     }
 }
