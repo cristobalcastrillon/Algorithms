8100a3416a38fa9cbe5d37056726ce08e08c2163
cristobalcastrillon <44101193+cristobalcastrillon@users.noreply.github.com>
2020-08-11 16:27:13 -0500

    Unsuccessfully tried to complete/implement the algorithm.

--- I. Introduction/MergeSort.cpp
+++ I. Introduction/MergeSort.cpp
@@ -2,26 +2,36 @@
 
 using namespace std;
 
-int* mergeSort(int array[]);
+int * mergeSort(int array[]);
 
 int main(){
     int array[] = {5,4,1,8,7,2,6,3};
 }
 
-int* mergeSort(int array[]) {
+int * mergeSort(int array[]) {
 
     int n = sizeof(array);
     int m = n/2;
-    //Declarando arrays.
-    //Tres arrays: 
-    //  * Dos de length: n = 1; A y B.
-    int * A = new int[m];
-    int * B = new int[m];
+
+    //Declarando arrays: 
+    //  * Dos de length: m = n/2; A y B.
+    int A[m];
+    int B[m];
+
+    //Poblando arrays...
+    //x es el indice para poblar A; y el indice para poblar B.
+    int x = 0, y = m;
+    for(int i = 0; i < m; i++){
+        A[i] = array[x];
+        B[i] = array[y];
+        x++;
+        y++;
+    }
 
     if(m == 1) {
         int i = 0, j = 0;
-        //  * Uno de length: h = 2n; C.
 
+        //  * Uno de length: n = 2 * m; C.
         int * C = new int[n];
 
         for(int k = 0; k < n; k++){
@@ -35,5 +45,5 @@ int* mergeSort(int array[]) {
             }
         }
         return C;
-    }    
+    }
 }
