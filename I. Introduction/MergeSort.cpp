98b45f0699c3ab590478a87c1ab77cfeb0d1025e
cristobalcastrillon <cristobalcastrillon@gmail.com>
2020-08-12 02:41:39 -0500

    Haven't found the source of the error.

--- I. Introduction/MergeSort.cpp
+++ I. Introduction/MergeSort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void mergeSort(int * array, int n);
+void mergeSort(int * arrPtr, int n);
 void merge(int * a, int * b, int n);
 
 int main(){
@@ -10,8 +10,7 @@ int main(){
     int a[] = {5,4,1,8,7,2,6,3};
     int n = sizeof(a) / sizeof(int);
 
-    int * aPtr = NULL;
-    aPtr = new int[n];
+    int * arrPtr = a;
 
     cout << "a is an array of size " << n << ", with values ";
 
@@ -21,7 +20,7 @@ int main(){
 
     cout << endl;
 
-    mergeSort(a, n);
+    mergeSort(arrPtr, n);
     cout << "After the mergeSort, a is ";
     
     for(int i = 0; i < n ; i++){
@@ -29,6 +28,8 @@ int main(){
     }
 
     cout << endl;
+
+    return 0;
 }
 
 void mergeSort(int * array, int n) {
@@ -38,13 +39,11 @@ void mergeSort(int * array, int n) {
 
     //Declarando arrays: 
     //  * Dos de length: m = n/2; a y b.
-    int * a = NULL;
-    a = new int[m];
-    int * b = NULL;
-    b = new int [m];
+    int * a = new int[m];
+    int * b = new int [m];
 
     //Poblando arrays...
-    //x es el indice para poblar a; y el indice para poblar b.
+    //'x' es el indice para poblar a; 'y' el indice para poblar b.
     int x = 0, y = m;
     for(int i = 0; i < m; i++){
         *(a+i) = *(array+x);
@@ -56,18 +55,19 @@ void mergeSort(int * array, int n) {
     if(m == 1) {
         merge(a, b, n);
     } 
-    
 
     mergeSort(a, m);
     mergeSort(b, m);
+
+    delete [] a;
+    delete [] a;
 }
 
 void merge(int * a, int * b, int n) {
     int i = 0, j = 0;
 
     //  * Uno de length: n = 2 * m; c.
-    int * c = NULL;
-    c = new int[n];
+    int * c = new int[n];
 
     for(int k = 0; k < n; k++){
         if(*(a+i) < *(b+j)){
